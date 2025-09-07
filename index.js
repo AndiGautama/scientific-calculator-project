8da94bc8e11dc7fec72ca461564f5307a3081f8e
VincentWu503 <vincenthendry06@gmail.com>
2025-09-07 23:04:32 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let display = document.getElementById("display");
+
+let isRadian = false;
+let isDegree = false;
+
+function isRadian(){
+    isRadian = true;
+    isDegree = false;
+}
+
+function isDegree(){
+    isRadian = false;
+    isDegree = true;
+}
